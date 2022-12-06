fa04728ff6c6c26eacc567a33733609d6d68da91
senghuyjr11 <senghuymit007@gmail.com>
2022-12-06 10:52:52 -0500

    First Init

--- src/main/java/com/example/springbootrestapicrud/controller/BookController.java
+++ src/main/java/com/example/springbootrestapicrud/controller/BookController.java
@@ -1,2 +1,19 @@
-package com.example.springbootrestapicrud.controller;public class BookController {
+package com.example.springbootrestapicrud.controller;
+
+import com.example.springbootrestapicrud.domain.Book;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/")
+public class BookController {
+    @PostMapping
+    private Book create(@RequestBody Book book) {
+        book.setId(1);
+        book.setTitle("Kotlin");
+        book.setCreatedDate(LocalDate.now());
+        book.setModifiedDate(LocalDate.now());
+        return book;
+    }
 }
--- src/main/java/com/example/springbootrestapicrud/domain/Book.java
+++ src/main/java/com/example/springbootrestapicrud/domain/Book.java
@@ -1,2 +1,26 @@
-package com.example.springbootrestapicrud.domain;public class Book {
+package com.example.springbootrestapicrud.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity(name = "book")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private LocalDate createdDate;
+    private LocalDate modifiedDate;
 }
